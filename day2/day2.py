090d2269dbf06327b2c0a27dcc30107a0be17438
septadwicahyo <a710230053@student.ums.ac.id>
2024-06-06 20:10:51 +0700

    Create day2.py

---
+++ day2/day2.py
@@ -0,0 +1,9 @@
+#meminta inputan user
+nama = input('Masukkan nama: ')
+nim = input ('Masukkan NIM: ')
+kelas = input ('Masukkan kelas: ')
+
+#mencetak input dari user
+print(f"Nama saya {nama}")
+print(f"NIM saya {nim}")
+print(f"Dari kelas {kelas}")
