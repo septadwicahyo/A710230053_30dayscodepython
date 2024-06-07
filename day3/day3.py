e71ed44d2bfdff007de7040e32e2956ccd779ce7
septadwicahyo <a710230053@student.ums.ac.id>
2024-06-07 20:42:41 +0700

    Create day3.py

---
+++ day3/day3.py
@@ -0,0 +1,9 @@
+#Data dalam variabel
+nama = "Septa" 
+tinggi_badan = 174
+berat_badan = 62
+
+#Mengecek tipe data dalam variabel
+print(f'Tipe data dari data {nama} adalah {type(nama)}') # tipe data string (str)
+print(f'Tipe data dari data {tinggi_badan} adalah {type(tinggi_badan)}') # tipe data integer (int)
+print(f'Tipe data dari data {berat_badan} adalah {type(berat_badan)}') # tipe data float
