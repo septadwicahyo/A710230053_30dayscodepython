932ba9a6a438f0e396afaf85accff99a252da621
septadwicahyo <a710230053@student.ums.ac.id>
2024-06-08 20:20:54 +0700

    Create day4.py

---
+++ day4/day4.py
@@ -0,0 +1,8 @@
+string = "Halo, nama saya septa"
+
+print("panjang string = ", len(string))
+print("index pertama huruf s berada di ", string.index("s"))
+print("huruf a muncul sebanyak ", string.count("a"))
+print(string.upper())
+print(string.lower())
+print(string.split(" "))
